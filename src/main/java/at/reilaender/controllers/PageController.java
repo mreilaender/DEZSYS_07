ac467841d2cbebb399fd992c9dcc6ea502be63d0
BuildTools <mreilaender@student.tgm.ac.at>
2016-01-08 11:39:45 +0100

    Added comments for all methods

--- src/main/java/at/reilaender/controllers/PageController.java
+++ src/main/java/at/reilaender/controllers/PageController.java
@@ -18,12 +18,23 @@ public class PageController {
     @Autowired
     private PageTestDao pageTestDao;
 
+    /**
+     * Handles POST requests on the URL /pages
+     * @param pageTest Page Entry which will be created
+     * @return Created Page and HttpStatus.CREATED
+     */
     @RequestMapping(method = RequestMethod.POST, produces = "application/json")
     public ResponseEntity<PageTest> createPage(@RequestBody PageTest pageTest) {
         this.pageTestDao.save(pageTest);
         return new ResponseEntity<>(pageTest, HttpStatus.CREATED);
     }
 
+    /**
+     * Handles GET requests on the URL /pages
+     * Method is used to find Pages by the ID
+     * @param id Used to specify a specific Page
+     * @return Found Page and HttpStatus.FOUND
+     */
     @RequestMapping(method = RequestMethod.GET, produces = "application/json")
     public ResponseEntity<PageTest> findById(@RequestParam Integer id) {
         PageTest pageTest = null;
@@ -33,6 +44,14 @@ public class PageController {
         return new ResponseEntity<>(pageTest, HttpStatus.FOUND);
     }
 
+    /**
+     * Handles PUT requests on the URL /pages/{id}
+     * {id} is a placeholder for the id of a Page
+     * This method updates an existing Page Entry
+     * @param id Used to specify a specific Page
+     * @param pageTest Page Entry with the updated Columns
+     * @return Updated page and HttpStatus.OK
+     */
     @RequestMapping(value = "/{id}", method = RequestMethod.PUT, produces = "application/json")
     public ResponseEntity<PageTest> updatePage(@PathVariable Integer id, @RequestBody PageTest pageTest) {
         PageTest pageTestTmp = this.pageTestDao.findByPageId(id);
@@ -46,6 +65,13 @@ public class PageController {
         return new ResponseEntity<>(pageTestTmp, HttpStatus.OK);
     }
 
+    /**
+     * Handles DELETE requests on the URL /pages/{id}
+     * {id} is a placeholder for the id of a Page
+     * This method is used to delete an existing Page Entry
+     * @param id Used to specify a specific Page
+     * @return Deleted Page and HttpStatus.OK
+     */
     @RequestMapping(value = "/{id}", method = RequestMethod.DELETE, produces = "application/json")
     public ResponseEntity<PageTest> deletePageById(@PathVariable Integer id) {
         PageTest pageTest = null;
