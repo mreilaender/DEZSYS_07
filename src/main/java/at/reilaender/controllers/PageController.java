22daeab151408983a8a00a475b6a44b24772f100
BuildTools <mreilaender@student.tgm.ac.at>
2016-01-08 11:26:07 +0100

    bugfix

--- src/main/java/at/reilaender/controllers/PageController.java
+++ src/main/java/at/reilaender/controllers/PageController.java
@@ -6,13 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 /**
- * @author manue
- * @version 29.12.2015
+ * @author mreilaender
+ * @version 08.01.2015
  */
 @Controller
 @RequestMapping(value = "/pages")
@@ -21,42 +19,40 @@ public class PageController {
     private PageTestDao pageTestDao;
 
     @RequestMapping(method = RequestMethod.POST, produces = "application/json")
-    public ResponseEntity<PageTest> createPage(@RequestParam String title, @RequestParam(required = false) String description, @RequestParam String content) {
-        PageTest pageTest = new PageTest(title, description, content);
+    public ResponseEntity<PageTest> createPage(@RequestBody PageTest pageTest) {
         this.pageTestDao.save(pageTest);
         return new ResponseEntity<>(pageTest, HttpStatus.CREATED);
     }
 
     @RequestMapping(method = RequestMethod.GET, produces = "application/json")
-    public ResponseEntity<PageTest> find(@RequestParam Integer id) {
+    public ResponseEntity<PageTest> findById(@RequestParam Integer id) {
         PageTest pageTest = null;
         pageTest = this.pageTestDao.findByPageId(id);
         if(pageTest == null)
             return  new ResponseEntity<PageTest>(pageTest, HttpStatus.NOT_FOUND);
-        return new ResponseEntity<PageTest>(pageTest, HttpStatus.FOUND);
+        return new ResponseEntity<>(pageTest, HttpStatus.FOUND);
     }
 
-    @RequestMapping(method = RequestMethod.PUT, produces = "application/json")
-    public ResponseEntity<PageTest> updatePage(@RequestParam Integer id, @RequestParam(required = false) String title, @RequestParam(required = false) String description, @RequestParam(required = false) String content) {
-        PageTest pageTest = null;
-        pageTest = this.pageTestDao.findByPageId(id);
-        if(title != null)
-            pageTest.setPageTitle(title);
-        if(description != null)
-            pageTest.setPageDescription(description);
-        if(content != null)
-            pageTest.setPageContent(content);
+    @RequestMapping(value = "/{id}", method = RequestMethod.PUT, produces = "application/json")
+    public ResponseEntity<PageTest> updatePage(@PathVariable Integer id, @RequestBody PageTest pageTest) {
+        PageTest pageTestTmp = this.pageTestDao.findByPageId(id);
+        if(pageTest.getPageTitle() != null)
+            pageTestTmp.setPageTitle(pageTest.getPageTitle());
+        if(pageTest.getPageDescription() != null)
+            pageTestTmp.setPageDescription(pageTest.getPageDescription());
+        if(pageTest.getPageContent() != null)
+            pageTestTmp.setPageContent(pageTest.getPageContent());
         this.pageTestDao.save(pageTest);
-        return new ResponseEntity<PageTest>(pageTest, HttpStatus.OK);
+        return new ResponseEntity<>(pageTestTmp, HttpStatus.OK);
     }
 
-    @RequestMapping(method = RequestMethod.DELETE, produces = "application/json")
-    public ResponseEntity<PageTest> deletePage(@RequestParam Integer id) {
+    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE, produces = "application/json")
+    public ResponseEntity<PageTest> deletePageById(@PathVariable Integer id) {
         PageTest pageTest = null;
         pageTest = this.pageTestDao.findOne(id);
         if(pageTest == null)
             return new ResponseEntity<PageTest>(pageTest, HttpStatus.NOT_FOUND);
         this.pageTestDao.delete(pageTest);
-        return new ResponseEntity<PageTest>(pageTest, HttpStatus.OK);
+        return new ResponseEntity<>(pageTest, HttpStatus.OK);
     }
 }
